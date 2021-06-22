99c816de94e40d4a9fb80dbf270d5b43d01aa6e9
mxr <eetu.kivelae@gmail.com>
2021-06-23 02:39:13 +0300

    Command: Added time bomb to pena command

--- src/commands/pena.rs
+++ src/commands/pena.rs
@@ -5,6 +5,9 @@ use serenity::{
   model::channel::Message,
 };
 
+use rand::Rng;
+use regex::Regex;
+
 
 pub struct PenaCommand {
   name: &'static str,
@@ -25,12 +28,20 @@ impl Command for PenaCommand {
   }
 
   fn requirement(&self, _ctx: &Context, msg: &Message) -> bool {
-    msg.content == "pena"
+    let rgx = Regex::new(r"[P|p][E|e][N|n][A|a]").unwrap();
+    if rgx.is_match(&msg.content) {
+      rand::thread_rng().gen_bool(0.03)
+    } else {
+      false
+    }
   }
 
   async fn action(&self, ctx: Context, msg: Message) {
-    if let Err(err) = msg.channel_id.say(&ctx.http, "<@855177115104575518> mis see on").await {
-      println!("Error: {:?}", err);
+    for _ in 0..5 {
+      let content = format!("<@{}> mis see on", msg.author.id);
+      if let Err(err) = msg.channel_id.say(&ctx.http, content).await {
+        println!("Error: {:?}", err);
+      }
     }
     self.log(ctx, msg);
   }
