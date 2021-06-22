94ac4477ccaba035f03cfb2706c442e2a98fc3e2
mxr <eetu.kivelae@gmail.com>
2021-06-22 03:49:16 +0300

    Clusterfuck: Added a bunch of stuff for future use

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ use dotenv;
 use std::env;
 use tokio;
 
+use songbird::SerenityInit;
+
 use serenity::{
   async_trait,
+  client::Context,
   client::bridge::gateway::GatewayIntents,
   model::{channel::Message, gateway::Ready, gateway::Activity},
   prelude::*,
@@ -19,8 +22,81 @@ impl EventHandler for Handler {
         println!("Error: {:?}", err);
       }
     } else if msg.content == "pena" {
-      if let Err(err) = msg.channel_id.say(&ctx.http, "<@134032786611765248> mis see on").await {
+      if let Err(err) = msg.channel_id.say(&ctx.http, "<@855177115104575518> mis see on").await {
         println!("Error: {:?}", err);
+      }
+    } else if msg.content == "pena100" {
+      for _ in 0..100 {
+        if let Err(err) = msg.channel_id.say(&ctx.http, "<@855177115104575518> mis see on").await {
+          println!("Error: {:?}", err);
+        }
+      }
+    } else if msg.content == "&join" {
+      let guild = msg.guild(&ctx.cache).await.unwrap();
+      let guild_id = guild.id;
+
+      let channel_id = guild
+        .voice_states.get(&msg.author.id)
+        .and_then(|vs|vs.channel_id);
+
+      let connect_to = match channel_id {
+        Some(ch) => ch,
+        None => {
+          if let Err(err) = msg.reply(&ctx.http, "Mis see on").await {
+            println!("Error: {:?}", err);
+          }
+          return;
+        }
+      };
+
+      let manager = songbird::get(&ctx).await
+        .expect("Songbird client error").clone();
+
+      let _handler = manager.join(guild_id, connect_to).await;
+    } else if msg.content == "&leave" {
+      let guild = msg.guild(&ctx.cache).await.unwrap();
+      let guild_id = guild.id;
+
+      let manager = songbird::get(&ctx).await
+        .expect("Songbird client error").clone();
+
+      if manager.get(guild_id).is_some() {
+        if let Err(err) = manager.remove(guild_id).await {
+          println!("Error {:?}", err);
+        }
+      } else {
+        if let Err(err) = msg.reply(&ctx.http, "Mis see on").await {
+          println!("Error: {:?}", err);
+        }
+      }
+    } else if msg.content == "&test" {
+      let guild = msg.guild(&ctx.cache).await.unwrap();
+      let guild_id = guild.id;
+
+      let manager = songbird::get(&ctx).await
+        .expect("Songbird client error").clone();
+
+      if let Some(handler_lock) = manager.get(guild_id) {
+
+        let path = std::path::Path::new("./audio/Primogenitor_master-02.mp3");
+        let source = match songbird::ytdl("https://www.youtube.com/watch?v=FM_-DjMlnzE").await {
+          Ok(source) => source,
+          Err(err) => {
+            println!("Error: {:?}", err);
+            if let Err(err) = msg.channel_id.say(&ctx.http, "ffmpeg error").await {
+              println!("Error: {:?}", err);
+            }
+            return;
+          },
+        };
+
+        //println!("{:?}", source.seek_time(std::time::Duration::from_secs(0)));
+        let mut handler = handler_lock.lock().await;
+        let handle = handler.play_source(source);
+        println!("{:?}", handle.metadata());
+        if let Err(err) = msg.channel_id.say(&ctx.http, "mis see on").await {
+          println!("Error: {:?}", err);
+        }
       }
     }
   }
@@ -43,6 +119,7 @@ async fn main() {
   let mut client = Client::builder(&token)
     .event_handler(Handler)
     .intents(GatewayIntents::GUILDS | GatewayIntents::GUILD_MESSAGES)
+    .register_songbird()
     .await
     .expect("Error creating client");
   
